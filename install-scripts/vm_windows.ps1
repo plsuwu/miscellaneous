3098bec783ff81cd4f4857a2aa59c70876888813
pls <zac.womal@outlook.com>
2024-01-23 02:44:12 +1100

    Create vm_windows.ps1

---
+++ install-scripts/vm_windows.ps1
@@ -0,0 +1,109 @@
+#Requires -RunAsAdministrator
+
+# ----------------------
+# untested & incomplete 
+# ----------------------
+
+param (
+    [switch] $NoPyverInstall = $false,
+    [switch] $NoOfficeInstall = $false
+)
+
+Set-ExecutionPolicy Bypass -Scope CurrentUser;
+$chocoPath = "C:\ProgramData\chocolatey\bin\"
+
+# -- pyenv & python vers --
+Write-Host " "
+Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"; &"./install-pyenv-win.ps1";
+
+$pyvers = @(
+    "3.12.1",
+    "3.11.6",
+    "3.10.6",
+    "3.6.8",
+    "2.7.18"
+)
+
+if (!($NoPyVerInstall)) {
+    foreach ($v in $pyvers) {
+        pyenv install $v
+    }
+    Write-Host "setting global python to 3.12.1..."
+    "pyenv global 3.12.1" | Invoke-Expression
+    Write-Host " "
+}
+
+# -- ms office & [redacted] --
+$OfficeUri="https://c2rsetup.officeapps.live.com/c2r/download.aspx?ProductreleaseID=O365ProPlusRetail&platform=x64&language=en-us&version=O16GA"
+
+if (!($NoOfficeInstall)) {
+    Set-Location $env:USERPROFILE\Desktop; Invoke-WebRequest -Uri $OfficeUri -MaximumRedirection 20 -OutFile "OfficeSetup.exe"; .\OfficeSetup.exe;
+    
+    # will have to manually do keypresses for now but im sure this can be automated
+    (Invoke-RestMethod https://massgrave.dev/get | Invoke-Expression) # 2 -> 1. 
+}
+
+# -- choco & packages --
+If (!(Test-Path $chocoPath)) {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')); 
+} else {
+    Write-Host "--------------------------------------------------------------------------------------------------"
+    Write-Host "[!] WARN: Chocolatey path found - assuming invalid installing and removing from machine. " -ForegroundColor Red
+    Write-Host "[!] WARN: Kill the session with `^C` in the next 3 seconds to cancel." -ForgroundColor Red
+    Write-Host "--------------------------------------------------------------------------------------------------`n"
+    Start-Sleep 1; Write-Host "3..."; Start-Sleep 1; Write-Host "2..."; Start-Sleep 1; Write-Host "1..."; Start-Sleep 1;
+    Write-Host "[*] Removing directory..."
+    Remove-Item -Recurse $chocoPath;
+    Write-Host "[*] Directory erased. Moving to clean-install choco:"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')); 
+}
+
+$binaries = @(
+    "chromium",
+    "systeminformer-nightlybuilds",
+    "microsoft-windows-terminal",
+    "neovim",
+    "neovide",
+    "vscode",
+    "ghidra",
+    "radare2",
+    "cutter",
+    "ida-free",
+    "7zip",
+    "cyberchef",
+    "git",
+    "netcat",
+    "sysinternals",
+    "powershell",
+    "powershell-core",
+    "adobereader",
+    "vcredist140",
+    "dotnetfx",
+    "dnspyex",
+    "ilspy",
+    "upx",
+    "ollydbg",
+    "x64dbg.portable",
+    "pestudio",
+    "osquery",
+    "sleuthkit",
+    "volatility3",
+    "floss",
+    "checksum"
+    )
+
+Foreach ($bin in $binaries) {
+    Invoke-Expression "choco install $bin -y"
+} 
+
+# -- manual downloads (will have to be installed by hand for now) --
+$SleuthkitUri="https://github.com/sleuthkit/sleuthkit/releases/download/sleuthkit-4.12.1/sleuthkit-4.12.1-win32.zip"
+Set-Location $env:USERPROFILE\Desktop; Invoke-WebRequest -Uri $SleuthkitUri -MaximumRedirection 20 -OutFile "sleuthkit.zip"; Invoke-Expression "7z x .\sleuthkit.zip"
+$AutopsyUri="https://github.com/sleuthkit/autopsy/releases/download/autopsy-4.21.0/autopsy-4.21.0-64bit.msi" 
+Set-Location $env:USERPROFILE\Desktop; Invoke-WebRequest -Uri $AutopsyUri -MaximumRedirection 20 -OutFile "Autopsy-4.21.0-64bit.msi"; ".\Autopsy-4.21.0-64bit.msi"
+$OLEDumpUri="https://didierstevens.com/files/software/oledump_V0_0_75.zip"
+Set-Location $env:USERPROFILE\Desktop; Invoke-WebRequest -Uri $OLEDumpUri -MaximumRedirection 20 -OutFile "OLEDump.zip"; Invoke-Expression "7z x .\OLEDump.zip"
+
+# very necessary
+$ImgZipUri="https://storage.googleapis.com/plsuwu_pub/img.zip"
+Invoke-WebRequest -Uri $ImgZipUri -MaximumRedirection 20 -OutFile "img.zip"; Invoke-Expression "7z x .\img.zip"
