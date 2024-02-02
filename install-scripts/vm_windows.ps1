75a9f611e503f05db4b21b7ef7781620d53b3e4e
pls <zac.womal@outlook.com>
2024-02-02 20:30:36 +1000

    Update vm_windows.ps1

--- install-scripts/vm_windows.ps1
+++ install-scripts/vm_windows.ps1
@@ -14,7 +14,7 @@ $chocoPath = "C:\ProgramData\chocolatey\bin\"
 
 # -- pyenv & python vers --
 Write-Host " "
-Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"; &"./install-pyenv-win.ps1"; . $PROFILE;
+Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"; &"./install-pyenv-win.ps1"; # . $PROFILE;
 
 $pyvers = @(
     "3.12.1",
